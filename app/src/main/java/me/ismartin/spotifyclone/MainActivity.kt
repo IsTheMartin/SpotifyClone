e2f6a430e0879c1670b466dd398508733749a9ee
Martin Morales <isthemartin@gmail.com>
2020-11-01 22:03:16 -0600

    Initializing project

---
+++ app/src/main/java/me/ismartin/spotifyclone/MainActivity.kt
@@ -0,0 +1,11 @@
+figpackage me.ismartin.spotifyclone
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
